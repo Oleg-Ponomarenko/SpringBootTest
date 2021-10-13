3ded8adffe3ea004855e0f2dd0c882eb35f9964d
ponomarenkoog.22 <ponomarenko1711@mail.ru>
2021-10-13 11:56:07 +0300

    Примеры с видами запросов

--- src/main/java/com/example/demo/ApiController.java
+++ src/main/java/com/example/demo/ApiController.java
@@ -1,15 +1,38 @@
 package com.example.demo;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-@Controller
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
 @RestController
 public class ApiController {
-    @GetMapping("books")
-    public ResponseEntity<String> getText() {
-        return ResponseEntity.ok("Hello text");
+
+    ArrayList<String> messages = new ArrayList<>();
+
+    @GetMapping("messages")
+    public ArrayList<String> getMessage() {
+        return messages;
+    }
+
+    @PostMapping("messages")
+    public void createMessage(@RequestBody String message) {
+        messages.add(message);
+    }
+
+    @GetMapping("messages/{index}")
+    public String getMessage(@PathVariable("index") Integer i) {
+        return messages.get(i);
+    }
+
+    @DeleteMapping("messages/{index}")
+    public void deleteMessage(@PathVariable("index") Integer i) {
+        messages.remove(i);
+    }
+
+    @PutMapping("messages/{index}")
+    public void updateMessage(@PathVariable("index") Integer i, @RequestBody String message) {
+        messages.remove(i);
+        messages.add(i, message);
     }
 }
 

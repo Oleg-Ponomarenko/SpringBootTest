fdafbddca91320a5766ff8a52f07c6b1f94b3ee2
Oleg <ponomarenko1711@mail.ru>
2021-10-20 06:11:58 +0300

    Добавлены примеры команд для работы с REST-запросами

--- src/main/java/com/example/demo/ApiController.java
+++ src/main/java/com/example/demo/ApiController.java
@@ -14,6 +14,8 @@ public class ApiController {
         return messages;
     }
 
+    //curl -X POST http://localhost:8080/messages -H 'Content-Type:
+    //text/plain' -d 'text'
     @PostMapping("messages")
     public void createMessage(@RequestBody String message) {
         messages.add(message);
@@ -24,11 +26,14 @@ public class ApiController {
         return messages.get(i);
     }
 
+    //curl -X DELETE http://localhost:8080/messages/1
     @DeleteMapping("messages/{index}")
     public void deleteMessage(@PathVariable("index") Integer i) {
         messages.remove(i);
     }
 
+    //curl -X PUT http://localhost:8080/messages/1 -H 'Content-Type:
+    //text/plain' -d 'text'
     @PutMapping("messages/{index}")
     public void updateMessage(@PathVariable("index") Integer i, @RequestBody String message) {
         messages.remove(i);
